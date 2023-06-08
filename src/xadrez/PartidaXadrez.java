d289850b8829bcfa4939ec121f195817fcfe0796
Adriana Olindo <adriana-olindo18@hotmail.com>
2023-06-08 11:45:56 -0300

    Melhora na impressão do tabuleiro

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -30,9 +30,20 @@ public class PartidaXadrez {
 	}
 
 	private void inicioPartida() {
-		novaPeca('b', 6, new Torre(tabuleiro, Color.WHITE));
-		novaPeca('e', 8, new Rei(tabuleiro, Color.WHITE));
-		novaPeca('e', 1, new Rei(tabuleiro, Color.WHITE));
+
+		novaPeca('c', 1, new Torre(tabuleiro, Color.WHITE));
+		novaPeca('c', 2, new Torre(tabuleiro, Color.WHITE));
+		novaPeca('d', 2, new Torre(tabuleiro, Color.WHITE));
+		novaPeca('e', 2, new Torre(tabuleiro, Color.WHITE));
+		novaPeca('e', 1, new Torre(tabuleiro, Color.WHITE));
+		novaPeca('d', 1, new Rei(tabuleiro, Color.WHITE));
+
+		novaPeca('c', 7, new Torre(tabuleiro, Color.BLACK));
+		novaPeca('c', 8, new Torre(tabuleiro, Color.BLACK));
+		novaPeca('d', 7, new Torre(tabuleiro, Color.BLACK));
+		novaPeca('e', 7, new Torre(tabuleiro, Color.BLACK));
+		novaPeca('e', 8, new Torre(tabuleiro, Color.BLACK));
+		novaPeca('d', 8, new Rei(tabuleiro, Color.BLACK));
 
 	}
 
